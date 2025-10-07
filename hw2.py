38b0ba6bdc5e22f75b3cf4f56926d79ee2d6e520
itayeylath <itay.ey@gmail.com>
2025-10-07 11:03:04 +0300

    add hw2.py

---
+++ hw2.py
@@ -0,0 +1,22 @@
+# >>> sum_of_squares([2,1,3])
+# 14
+# >>> half_list([2,4,6,8,9])
+# [2,6,9]
+# >>> pyramid(2) # Note: The function will print, and return None.
+# [1]
+# [2,2]
+# >>> flipped_pyramid(2) # Note: The function will print, and return None.
+# **
+# *
+# >>> min_len(["hello", "world!"])
+# "hello"
+# >>> sort_str(["hello", "dear", "world!"])
+# ["dear", "hello", "world!"]
+# >>> b_language("ani ohev otach")
+# "abanibi obohebev obotabach"
+# >>> multiply(4) # Note: The function will print, and return None.
+# [1,2,3,4]
+# [2,4,6,8]
+# [3,6,9,12]
+# [4,8,12,16]
+
